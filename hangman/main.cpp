8ecdd3aae49257657c1238e6f908bc4e35dabe68
CHPmelon <chpmelon@gmail.com>
2016-11-22 23:14:50 -0800

    Initial Commit

---
+++ hangman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hangman
+//
+//  Created by Kaleb Cervantes on 11/22/16.
+//  Copyright © 2016 Kaleb Cervantes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
